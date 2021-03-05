37c68ddf7b87903aba02f5e2756b67de03d98cc2
ricardojunior02 <ricardojunior199502@gmail.com>
2021-03-05 14:49:09 -0300

    Migration de Users

---
+++ src/database/migrations/1592846046726-CreateNormalUsers.ts
@@ -0,0 +1,83 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateNormalUsers1592846046726
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'cpf',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'street',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'street_number',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
