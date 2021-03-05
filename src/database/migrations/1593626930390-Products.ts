ca46201df9e054d77fa96a600f7cb128d62f8243
ricardojunior02 <ricardojunior199502@gmail.com>
2021-03-05 14:49:33 -0300

    migration de products

---
+++ src/database/migrations/1593626930390-Products.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+export default class Products1593626930390 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'category',
+            type: 'varchar',
+          },
+          {
+            name: 'stock',
+            type: 'numeric',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
